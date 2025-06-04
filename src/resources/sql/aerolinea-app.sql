d9fe27e485d2099add32d7b4abe41e095b5daefd
Mario Alejandro Hernandez Lopez <sams24484@gmail.com>
2025-06-03 17:03:50 -0700

    Termine las entidades en mysql

--- src/resources/sql/aerolinea-app.sql
+++ src/resources/sql/aerolinea-app.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS pasajero (
     nacionalidad VARCHAR(40),
     pasaporte VARCHAR(40),
     email VARCHAR(50),
-    telefono BIGINT
+    telefono VARCHAR(10)
 );
 
 CREATE TABLE IF NOT EXISTS aeropuerto (
@@ -21,11 +21,11 @@ CREATE TABLE IF NOT EXISTS aeropuerto (
 
 CREATE TABLE IF NOT EXISTS ruta (
 	id_ruta INT PRIMARY KEY AUTO_INCREMENT,
-    origen INT,
-    destino INT,
+    id_aeropuerto_origen INT,
+    id_aeropuerto_destino INT,
     duracion INT,
-    FOREIGN KEY (origen) REFERENCES aeropuerto(id_aeropuerto),
-    FOREIGN KEY (destino) REFERENCES aeropuerto(id_aeropuerto)
+    FOREIGN KEY (id_aeropuerto_origen) REFERENCES aeropuerto(id_aeropuerto),
+    FOREIGN KEY (id_aeropuerto_destino) REFERENCES aeropuerto(id_aeropuerto)
 );
 
 CREATE TABLE IF NOT EXISTS avion (
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS avion (
     modelo VARCHAR(20),
     fabricante VARCHAR(40),
     capacidad INT,
-    año_fabricacion DATETIME
+    año_fabricacion YEAR
 );
 
 CREATE TABLE IF NOT EXISTS vuelo (
@@ -42,7 +42,9 @@ CREATE TABLE IF NOT EXISTS vuelo (
     id_avion INT,
     fecha_salida DATETIME,
     fecha_llegada DATETIME,
-    estado VARCHAR(40)
+    estado VARCHAR(40),
+    FOREIGN KEY (id_ruta) REFERENCES ruta(id_ruta),
+    FOREIGN KEY (id_ruta) REFERENCES avion(id_avion)
 );
 
 CREATE TABLE IF NOT EXISTS empleado (
@@ -61,5 +63,49 @@ CREATE TABLE IF NOT EXISTS tripulacion (
     PRIMARY KEY (id_vuelo, id_empleado),
     FOREIGN KEY (id_vuelo) REFERENCES vuelo(id_vuelo),
     FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
-)
+);
 
+CREATE TABLE IF NOT EXISTS reserva (
+	id_reserva INT PRIMARY KEY AUTO_INCREMENT,
+    id_vuelo INT,
+    fecha_reserva DATETIME,
+    estado VARCHAR(50),
+    FOREIGN KEY (id_vuelo) REFERENCES vuelo(id_vuelo)
+);
+
+CREATE TABLE IF NOT EXISTS detalle_reserva (
+	id_reserva INT,
+    id_pasajero INT,
+    PRIMARY KEY (id_reserva, id_pasajero),
+    FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva),
+    FOREIGN KEY (id_pasajero) REFERENCES pasajero(id_pasajero)
+);
+
+CREATE TABLE IF NOT EXISTS pago (
+	id_pago INT PRIMARY KEY AUTO_INCREMENT,
+    id_reserva INT,
+    metodo_pago VARCHAR(50),
+    monto float,
+    fecha_pago DATETIME,
+    FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
+);
+
+CREATE TABLE IF NOT EXISTS checkin (
+	id_checkin INT PRIMARY KEY AUTO_INCREMENT,
+    id_pasajero INT,
+    id_reserva INT,
+    fecha_checkin DATETIME,
+    asiento VARCHAR(3),
+    puerta_embarque INT,
+    FOREIGN KEY (id_pasajero) REFERENCES pasajero(id_pasajero),
+    FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
+);
+
+CREATE TABLE IF NOT EXISTS equipaje (
+	id_equipaje INT PRIMARY KEY AUTO_INCREMENT,
+    id_checkin INT,
+    peso INT,
+    descripcion VARCHAR(250),
+    costo_extra INT,
+    FOREIGN KEY (id_checkin) REFERENCES checkin(id_checkin)
+)
